5562d9d986748ba1cfb1dc0bbd3412dfdaeafd44
krasotun <servrostov@gmail.com>
2023-06-18 18:55:35 +0300

    add brain gcd logic

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,3 +1,45 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
 
-console.log('Brain gcd is here');
+const gcd = (a, b) => {
+  let first = a;
+  let second = b;
+  let temp;
+
+  while (second !== 0) {
+    temp = second;
+    second = first % second;
+    first = temp;
+  }
+
+  return first;
+};
+
+console.log('Welcome to the Brain Games!');
+
+const userName = readlineSync.question('May I have your name?');
+console.log(
+  `Hello, ${userName}\nFind the greatest common divisor of given numbers.`,
+);
+
+let i = 0;
+
+do {
+  const first = Math.floor(Math.random() * 10);
+  const second = Math.floor(Math.random() * 10);
+  console.log(`Question: ${first} ${second}`);
+  const result = +readlineSync.question('Your answer:');
+
+  if (+result === gcd(first, second)) {
+    console.log('Correct!');
+    i += 1;
+  } else {
+    console.log(
+      `'${result}' is wrong answer ;(. Correct answer was '${gcd(
+        first,
+        second,
+      )}' \nLet's try again, ${userName}`,
+    );
+    break;
+  }
+} while (i < 3);
