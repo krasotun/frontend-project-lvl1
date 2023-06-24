36ce1476e2a72f51902ce8799cd67853f6565b0f
krasotun <servrostov@gmail.com>
2023-06-24 21:07:00 +0300

    agan fix

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export default function sayHello() {
-  console.log('Welcome to the Brain Games!');
+  // console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May i have you name?');
 
   console.log(`Hello, ${userName}!`);
