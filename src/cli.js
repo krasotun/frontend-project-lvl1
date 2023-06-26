1bbe8723aafb1db5385f50b9c899565972e0e11b
krasotun <servrostov@gmail.com>
2023-06-26 20:31:21 +0300

    fix your

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 export default function sayHello() {
   // console.info('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May i have you name?');
+  const userName = readlineSync.question('May i have your name?');
 
   console.info(`Hello, ${userName}!`);
 }
