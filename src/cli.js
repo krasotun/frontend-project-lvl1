1c400937791fec2c5dc021461097f4b633634379
krasotun <servrostov@gmail.com>
2023-06-26 20:35:00 +0300

    fix welcome

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export default function sayHello() {
-  // console.info('Welcome to the Brain Games!');
+  console.info('Welcome to the Brain Games!');
   const userName = readlineSync.question('May i have your name?');
 
   console.info(`Hello, ${userName}!`);
